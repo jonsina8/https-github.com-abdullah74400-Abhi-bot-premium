81a0704472903d5e5c89f11df3dd3f82d2a93828
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2026-02-23 19:38:05 +0530

    Update database URI and name in info.py

--- info.py
+++ info.py
@@ -51,8 +51,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ABHISHEK:ABHISHEK@cluster0.r3q5bl9.mongodb.net/?appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://kari41875:ULpuG17sXjXV0Yna@cluster0.olegt6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Abhi12")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://ABHISHEK:ABHISHEK@cluster0.r3q5bl9.mongodb.net/?appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "ABHISHEK")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
